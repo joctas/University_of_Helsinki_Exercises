6c29bb7aed87b97b830c8f782bbec41882eb4155
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-10-29 23:57:41 -0300

    1.2: Course Information, step 2 -> Done!

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 const Header = (props) => {
   return (
     <div>
@@ -9,14 +10,18 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
+        {props.part} {props.exercises}
       </p>
     </div>
   )
